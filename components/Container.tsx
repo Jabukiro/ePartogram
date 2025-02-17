b48ec5922cfe2931d3a3bcdf6d0b3798877345af
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2025-02-17 20:42:45 +0200

    Revert background color

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,14 +3,14 @@ import { View, ViewProps, ViewStyle } from 'react-native';
 import { useAppTheme } from '@/app/_layout';
 
 interface Props extends ViewProps {
-    full: boolean,
+    full?: boolean,
 }
 export default function MainContainer({ full = true, ...rest }: Props): React.ReactNode {
     const theme = useAppTheme();
     return (
         <View style={{
             ...(typeof rest.style == "object" ? rest.style : {}),
-            backgroundColor: "#aaa", marginHorizontal: 10, paddingHorizontal: 50, paddingVertical: theme.spacing.p_2, height: full ? "100%" : undefined
+            backgroundColor: "#fff", paddingHorizontal: theme.spacing.p_2, paddingVertical: theme.spacing.p_2, height: full ? "100%" : undefined
         }} {...rest} />
     )
 }
