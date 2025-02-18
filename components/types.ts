c2e5a576fd163579bb013495b34bfab45a260333
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2025-02-18 19:59:48 +0200

    Add Patient type

--- components/types.ts
+++ components/types.ts
@@ -14,3 +14,24 @@ export interface HealthWorker {
     district: "",
     address: "",
 }
+export type PatientDemographics = {
+    first_name: string
+    surname: string
+    dob: string
+    marital_status: string
+    country: string
+    province: Province
+    district: string
+    suburb: string
+    literacy: string
+    religion: string
+}
+export type Patient = {
+    demographics: PatientDemographics,
+    status: "Alert" | "Attention",
+    data: Object
+}
+const patientDemKeys = ["marital_status", "country", "province", "district", "suburb", "literacy", "religion"]
+export function isPatient(obj: Object): obj is PatientDemographics{
+    return patientDemKeys.every((key)=>obj.hasOwnProperty(key))
+}
