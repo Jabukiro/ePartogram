fd1abf775297db8ab619d3775a539213180d68e8
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2025-02-19 20:37:01 +0200

    Fix require cycle

--- components/index.ts
+++ components/index.ts
@@ -3,5 +3,4 @@ import MainContainer from './Container';
 import MyTextInput from './TextInput';
 import MyButton from './Button';
 import PatientHomeCard from './PatientCard';
-import PressableGeneral from './Pressable';
-export {Clock, PatientHomeCard ,PressableGeneral,MainContainer, MyButton, MyTextInput};
+export {Clock, PatientHomeCard, MainContainer, MyButton, MyTextInput};
