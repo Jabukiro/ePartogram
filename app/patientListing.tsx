dcab7c3ffcc5bb9b62d619973ceb5b0dda7bf6c8
Herve-Daniel Barihuta <d.barihuta@gmail.com>
2025-02-19 20:38:59 +0200

    Open partograph on Expand

--- app/patientListing.tsx
+++ app/patientListing.tsx
@@ -3,14 +3,15 @@ import { Button, FlatList } from 'react-native';
 import { Clock, MainContainer, PatientHomeCard } from '@/components';
 import { __clear, retrieveActivePatients } from '@/api';
 import { Patient, StateProps } from '@/components/types';
+import { router } from 'expo-router';
 
 export default function PatientListingScreen() {
     const [activePatients, setActivePatients]: StateProps<Patient[] | undefined> = React.useState();
     React.useEffect(() => {
-        retrieveActivePatients().then(value => { setActivePatients(value); });
+        retrieveActivePatients().then(value => { console.log("Item to pateintcard", typeof value, value); setActivePatients(value); }, reason => { console.log("retreive failed with reason, ", reason) });
     }, [retrieveActivePatients]);
     const renderItem = ({ item }: { item: Patient }) => {
-        return <PatientHomeCard patient={item} />;
+        return <PatientHomeCard patient={item} onExpand={() => { router.push("./partograph") }} />;
     }
     return (
         <MainContainer>
